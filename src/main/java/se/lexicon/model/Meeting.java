96dd93db86a93292ea642be92a7db87db558a2d6
belavirag <139229267+belavirag@users.noreply.github.com>
2023-10-02 09:59:17 +0200

    impl Meeting & MeetingCalendar

--- src/main/java/se/lexicon/model/Meeting.java
+++ src/main/java/se/lexicon/model/Meeting.java
@@ -1,6 +1,49 @@
 package se.lexicon.model;
 
-public class Meeting {
-    // todo:
+import java.time.LocalDateTime;
 
+public class Meeting {
+    private int id;
+    private String title, description;
+    private LocalDateTime endTime, startTime;
+    private MeetingCalendar calendar;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public MeetingCalendar getCalendar() {
+        return calendar;
+    }
+
+    public void displayMeetingInfo() {
+        System.out.println(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Meeting{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", endTime=" + endTime +
+                ", startTime=" + startTime +
+                '}';
+    }
 }
--- src/main/java/se/lexicon/model/MeetingCalendar.java
+++ src/main/java/se/lexicon/model/MeetingCalendar.java
@@ -1,6 +1,48 @@
 package se.lexicon.model;
 
-public class MeetingCalendar {
+import java.util.List;
 
-    // todo:
+public class MeetingCalendar {
+    private int id;
+    private String title, username;
+    private List<Meeting> meetings;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<Meeting> getMeetings() {
+        return meetings;
+    }
+
+    public void addMeeting(Meeting meeting) {
+        this.meetings.add(meeting);
+    }
+
+    public void removeMeeting(Meeting meeting) {
+        this.meetings.remove(meeting);
+    }
+
+    public void displayCalendarInfo() {
+        System.out.println("Meeting calendar info: " + this.toString());
+        System.out.println("Meeting list:");
+        this.meetings.forEach(System.out::println);
+    }
+
+    @Override
+    public String toString() {
+        return "MeetingCalendar{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
 }
