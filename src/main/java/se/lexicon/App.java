a2ec80ac67fed7adce90f9e33e4cdf0a7840cfea
belavirag <139229267+belavirag@users.noreply.github.com>
2023-10-03 12:26:53 +0200

    impl MeetingCalendarDao

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,11 +1,14 @@
 package se.lexicon;
 
+import se.lexicon.dao.MeetingCalendarDao;
 import se.lexicon.dao.UserDao;
+import se.lexicon.dao.impl.MeetingCalendarDaoImpl;
 import se.lexicon.dao.impl.UserDaoImpl;
 import se.lexicon.dao.impl.db.MeetingCalendarDbConnection;
 import se.lexicon.exception.AuthenticationFailedException;
 import se.lexicon.exception.CalendarExceptionHandler;
 import se.lexicon.exception.UserExpiredException;
+import se.lexicon.model.MeetingCalendar;
 import se.lexicon.model.User;
 
 import java.util.Optional;
@@ -18,15 +21,20 @@ public class App {
 
         try {
             UserDao userDao = new UserDaoImpl(MeetingCalendarDbConnection.getConnection());
+            MeetingCalendarDao meetingCalendarDao = new MeetingCalendarDaoImpl(MeetingCalendarDbConnection.getConnection());
+
             //User createdUser = userDao.createUser("admin");
+            //MeetingCalendar calendar = meetingCalendarDao.createMeetingCalendar("Test meeting calendar", "admin");
+            //MeetingCalendar calendar2 = meetingCalendarDao.createMeetingCalendar("Another meeting calendar", "admin");
+
             //System.out.println("createdUser.userInfo() = " + createdUser.userInfo());
             //Optional<User> userOptional = userDao.findByUsername("admin");
             //if (userOptional.isPresent()){
             //    System.out.println(userOptional.get().userInfo());
             //}
 
-            boolean isAuthenticate = userDao.authenticate(new User("1234", "1234"));
-            System.out.println("You are logged in...");
+            //boolean isAuthenticate = userDao.authenticate(new User("1234", "1234"));
+            //System.out.println("You are logged in...");
         } catch (Exception e) {
             CalendarExceptionHandler.handelException(e);
 
---
+++ src/main/java/se/lexicon/dao/impl/MeetingCalendarDaoImpl.java
@@ -0,0 +1,121 @@
+package se.lexicon.dao.impl;
+
+import se.lexicon.dao.MeetingCalendarDao;
+import se.lexicon.exception.MySQLException;
+import se.lexicon.model.MeetingCalendar;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public class MeetingCalendarDaoImpl implements MeetingCalendarDao {
+    private Connection connection;
+
+    public MeetingCalendarDaoImpl(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public MeetingCalendar createMeetingCalendar(String title, String username) {
+        try (
+                PreparedStatement stmt = connection.prepareStatement("INSERT INTO MEETING_CALENDARS(USERNAME, TITLE) VALUES(?, ?)", PreparedStatement.RETURN_GENERATED_KEYS)
+        ) {
+            stmt.setString(1, username);
+            stmt.setString(2, title);
+
+            int rowsInserted = stmt.executeUpdate();
+            if (rowsInserted == 0) {
+                throw new MySQLException("Creation of meeting calendar failed, no rows affected.");
+            }
+
+            try (ResultSet generated = stmt.getGeneratedKeys()) {
+                if (generated.next()) {
+                    return new MeetingCalendar(generated.getInt(1), username, title);
+                } else {
+                    throw new MySQLException("No generated keys returned from database while inserting new meeting calendar.");
+                }
+            }
+        } catch (SQLException e) {
+            throw new MySQLException("Error occurred while creating meeting calendar: username= " + username + ", title=" + title, e);
+
+        }
+    }
+
+    @Override
+    public Optional<MeetingCalendar> findById(int id) {
+        try (
+                PreparedStatement stmt = connection.prepareStatement("SELECT * FROM MEETING_CALENDARS WHERE ID = ?")
+        ) {
+            stmt.setInt(1, id);
+
+            try(ResultSet result = stmt.executeQuery()) {
+                if (result.next()) {
+                    String title = result.getString("TITLE");
+                    String username = result.getString("USERNAME");
+                    return Optional.of(new MeetingCalendar(id, username, title));
+                } else {
+                    return Optional.empty();
+                }
+            }
+        } catch (SQLException e) {
+            throw new MySQLException("Error occurred while finding meeting calendar by id: " + id, e);
+        }
+    }
+
+    @Override
+    public Collection<MeetingCalendar> findByUsername(String username) {
+        List<MeetingCalendar> result = new ArrayList<>();
+
+        try (
+                PreparedStatement stmt = connection.prepareStatement("SELECT * FROM MEETING_CALENDARS WHERE USERNAME = ?")
+        ) {
+            stmt.setString(1, username);
+            try(ResultSet resultSet = stmt.executeQuery()) {
+                while (resultSet.next()) {
+                    String title = resultSet.getString("TITLE");
+                    String usernameFromDB = resultSet.getString("USERNAME");
+                    int id = resultSet.getInt("ID");
+
+                    result.add(new MeetingCalendar(id, username, title));
+                }
+            }
+        } catch (SQLException e) {
+            throw new MySQLException("Error occurred while finding meeting calendar by username: " + username, e);
+        }
+
+        return result;
+    }
+
+    @Override
+    public Optional<MeetingCalendar> findByTitle(String title) {
+        try (
+                PreparedStatement stmt = connection.prepareStatement("SELECT * FROM MEETING_CALENDARS WHERE TITLE = ?")
+        ) {
+            stmt.setString(1, title);
+
+            try(ResultSet result = stmt.executeQuery()) {
+                if (result.next()) {
+                    String titleFromDB = result.getString("TITLE");
+                    String username = result.getString("USERNAME");
+                    int id = result.getInt("ID");
+
+                    return Optional.of(new MeetingCalendar(id, username, titleFromDB));
+                } else {
+                    return Optional.empty();
+                }
+            }
+        } catch (SQLException e) {
+            throw new MySQLException("Error occurred while finding meeting calendar by title: " + title, e);
+        }
+    }
+
+    @Override
+    public boolean deleteCalendar(int id) {
+        return false;
+    }
+}
